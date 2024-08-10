39ce0c5cfc0d38dd3525430e554a09499f9059b5
NajelT <66961462+NajelT@users.noreply.github.com>
2024-08-10 15:50:00 +0300

    Initial commit

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface FrameSignaturePacket {
+  untrustedData: {
+    fid: number;
+    url: string;
+    messageHash: string;
+    timestamp: number;
+    network: number;
+    buttonIndex: number;
+    inputText?: string;
+    castId: {
+      fid: number;
+      hash: string;
+    };
+  };
+  trustedData: {
+    messageBytes: string;
+  };
+}
